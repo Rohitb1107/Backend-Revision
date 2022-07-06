5344d4af42353bc5e4d411519fdff63b9fbbb9e5
Rohit <stylishrb711@gmail.com>
2022-07-06 13:32:05 +0530

    server start running

--- Intro-to-express/calculator.js
+++ Intro-to-express/calculator.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+// const express = require("express");
+// const bodyParser = require("body-parser");
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
-app.post("/", (req, res) => {
-  var num1 = +req.body.num1;
-  var num2 = +req.body.num2;
-  var ans = num1 + num2;
+// app.post("/", (req, res) => {
+//   var num1 = +req.body.num1;
+//   var num2 = +req.body.num2;
+//   var ans = num1 + num2;
 
-  res.send("The result of your calculation is " + ans);
-});
+//   res.send("The result of your calculation is " + ans);
+// });
 
-app.listen(3000, () => {
-  console.log("Server starts running at port 3000.");
-});
+// app.listen(3000, () => {
+//   console.log("Server starts running at port 3000.");
+// });
