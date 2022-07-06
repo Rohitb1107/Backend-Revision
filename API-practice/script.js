54234fa652568ff0aff5ac26c08bbbeb30397ffe
Rohit <stylishrb711@gmail.com>
2022-07-06 12:16:01 +0530

    js

--- API-practice/script.js
+++ API-practice/script.js
@@ -1,11 +1,18 @@
-function getData() {
-  fetch("https://fakestoreapi.com/products")
-    .then((res) => {
-      res.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
-}
+// Simple fetch request
+// function getData() {
+//   fetch("https://fakestoreapi.com/products")
+//     .then((res) => res.json())
+//     .then((res) => console.log(res));
+// }
 
-getData();
+// getData();
+
+// Asynchronous
+// async function getData() {
+//   const API = "https://fakestoreapi.com/products";
+//   var res = await fetch(API);
+//   var data = await res.json();
+//   console.log(data);
+// }
+
+// getData();
