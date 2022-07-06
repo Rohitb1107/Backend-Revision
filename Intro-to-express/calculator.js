18345f4548b3b519094e09dfc9d88defb7707335
Rohit <stylishrb711@gmail.com>
2022-07-06 11:35:03 +0530

    works!

--- Intro-to-express/calculator.js
+++ Intro-to-express/calculator.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", (req, res) => {
-  res.send("Thanks for your submission!");
+  var num1 = +req.body.num1;
+  var num2 = +req.body.num2;
+  var ans = num1 + num2;
+
+  res.send("The result of your calculation is " + ans);
 });
 
 app.listen(3000, () => {
