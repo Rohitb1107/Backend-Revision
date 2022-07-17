9ca0a45fc577311e85ace33915f80b7ce260802d
Rohit <stylishrb711@gmail.com>
2022-07-17 22:41:46 +0530

    name update

--- Race-Api/src/app.js
+++ Race-Api/src/app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 require("./db/connect");
-const router = require("./controllers/mens.router");
+const mensRouter = require("./controllers/mens.router");
 
 app.use(express.json());
-app.use(router);
+app.use(mensRouter);
 
 app.listen(port, () => {
   console.log(`Server start running on port ${port}.`);
