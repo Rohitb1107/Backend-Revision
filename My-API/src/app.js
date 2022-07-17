d634f9ae836fb673d54f341747f0568c3c456f7e
Rohit <stylishrb711@gmail.com>
2022-07-17 19:51:03 +0530

    app update

--- My-API/src/app.js
+++ My-API/src/app.js
@@ -6,6 +6,7 @@ const Student = require("../models/students");
 
 app.use(express.json());
 
+// POST data
 app.post("/students", async (req, res) => {
   try {
     const user = new Student(req.body);
@@ -16,6 +17,27 @@ app.post("/students", async (req, res) => {
   }
 });
 
+// GET all students data
+app.get("/students", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    res.status(200).send(studentsData);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// GET individual students data
+app.get("/students/:id", async (req, res) => {
+  try {
+    const studentData = await Student.findById(req.params.id);
+    res.status(200).send(studentData);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Listen to server
 app.listen(port, () => {
   console.log(`Server start running on port ${port}.`);
 });
