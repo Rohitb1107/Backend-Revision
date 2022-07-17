e0499bd5afca2a53145f4a421ee18cd6be69b508
Rohit <stylishrb711@gmail.com>
2022-07-17 22:20:15 +0530

    get works!

--- Race-Api/src/app.js
+++ Race-Api/src/app.js
@@ -3,10 +3,28 @@ const app = express();
 const port = process.env.PORT || 8080;
 require("./db/connect");
 
-app.get("/", async (req, res) => {
-  res.send("Hello, World!");
+app.use(express.json());
+
+const MensRanking = require("./models/mens.model");
+
+app.post("/mens", async (req, res) => {
+  try {
+    const addMensData = new MensRanking(req.body);
+    const addDataToDB = await addMensData.save();
+    res.status(201).send(addDataToDB);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
+app.get("/mens", async (req, res) => {
+  try {
+    const getMensData = await MensRanking.find();
+    res.status(200).send(getMensData);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 app.listen(port, () => {
   console.log(`Server start running on port ${port}.`);
 });
